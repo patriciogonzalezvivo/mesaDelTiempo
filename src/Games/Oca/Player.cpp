8f93a60902eaf3b4cb4cc0fa3e2ece3e15fb0eb9
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2012-06-13 11:43:22 -0300

    fichas

--- src/Games/Oca/Player.cpp
+++ src/Games/Oca/Player.cpp
@@ -1,9 +1,27 @@
 //
 //  Player.cpp
-//  mdt-Core
+//  MesaDelTiempo for http://web.museodeljuguetesi.org.ar/
 //
-//  Created by Patricio González Vivo on 6/12/12.
-//  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
+//  Created by Patricio Gonzalez Vivo on 4/1/12.
+//  Copyright (c) 2012 http://PatricioGonzalezVivo.com All rights reserved.
 //
 
-#include <iostream>
+#include "Player.h"
+
+Player::Player(){
+    width = 30;
+    height = 30;
+    
+    color.set(255,255);
+}
+
+void Player::update(){
+    
+}
+
+void Player::draw(){
+    ofPushStyle();
+    ofSetColor(color);
+    ofCircle(getCenter(), (width+height)*0.5 );
+    ofPopStyle();
+}
--- src/Games/Oca/Player.h
+++ src/Games/Oca/Player.h
@@ -1,14 +1,32 @@
 //
 //  Player.h
-//  mdt-Core
+//  MesaDelTiempo for http://web.museodeljuguetesi.org.ar/
 //
-//  Created by Patricio González Vivo on 6/12/12.
-//  Copyright (c) 2012 PatricioGonzalezVivo.com. All rights reserved.
+//  Created by Patricio Gonzalez Vivo on 4/1/12.
+//  Copyright (c) 2012 http://PatricioGonzalezVivo.com All rights reserved.
 //
 
-#ifndef mdt_Core_Player_h
-#define mdt_Core_Player_h
+#ifndef PLAYER
+#define PLAYER
 
+#include "ofMain.h"
 
+class Player : public ofRectangle {
+public:
+    
+    Player();
+    
+    void    update();
+    void    draw();
+    
+    ofColor color;
+    
+    int     nId;
+    int     nPlace;
+    
+private:
+    ofImage *img;
+    
+};
 
 #endif
